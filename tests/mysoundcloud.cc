60b4f3d3d5767f957c102284a6f9d7174a5b895a
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2014-11-12 18:51:35 +0100

    Add mysoundcloud API prototype.

---
+++ tests/mysoundcloud.cc
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <iod/sio_utils.hh>
+#include <silicon/mimosa_backend.hh>
+#include <silicon/sqlite.hh>
+#include <silicon/server.hh>
+
+#include "mysoundcloud_symbols.hh"
+
+using namespace s;
+using namespace iod;
+
+typedef decltype(iod::D(_Id(_Primarykey) = int(),
+                        _Email = std::string(),
+                        _Password = std::string()
+                   )) User;
+
+typedef decltype(iod::D(_Id(_Primarykey) = int(),
+                        _Title = std::string(),
+                        _Filename = std::string()
+                   )) Song;
+
+
+struct session_data : public decltype(D(_User_id = int()))
+{
+  session_data() { user_id = -1; }
+  bool is_authenticated() const { return user_id != -1; }
+};
+
+struct authenticator
+{
+  bool login(const std::string& email, const std::string& password)
+  {
+    User u;
+    if (con("SELECT * from user where email == ? and password == ?", email, password) >> u and u.confirmed)
+    {
+      sess.user_id = user.id;
+      sess.save();
+      return true;
+    }
+  };
+
+  bool logout()
+  {
+    sess.user_id = -1;
+    sess.destroy();
+  }
+  
+  static auto instantiate(sqlite_session<session_data>& sess,
+                          sqlite_connection& con)
+  {
+    return authenticator{sess, con};
+  }
+
+  sqlite_session<session_data>& sess;
+  sqlite_connection& con;
+};
+
+struct current_user : public User
+{
+  static auto instantiate(session& sess, sqlite_orm& orm)
+  {
+    if (!sess.authenticated())
+      throw error::unauthorized("Access to this procedure is reserved to logged users.");
+
+    current_user u;
+    if (!orm.find_by_id(sess.user_id, u))
+      throw error::internal_server_error("Session user_id not in user table.");
+
+    return u;
+  }
+};
+
+int main(int argc, char* argv[])
+{
+  using namespace iod;
+
+  if (!argc)
+  {
+    std::cout << "Usage: " << argv[0] << " sqlite_database_path server_port" << std::endl;
+    return 1;
+  }
+  
+  auto server = silicon(sqlite_middleware(argv[1]),
+                        sqlite_session_storage("user_sessions"),
+                        sqlite_orm(Song(), "songs",
+                                   User(), "users"));
+
+  setup_CRUD<Song>(server,
+                   _Namespace = "song",
+                   _Write_access = [] (current_user& user, Song& song) { return song.user_id == user.id });
+
+  setup_CRUD<User>(server,
+                   _Namespace = "user",
+                   _Write_access = [] (current_user& cuser, User& user) { return cuser.id == user.id });
+
+  server["upload"] = [] (decltype(D(_Id = int(), _File = binary_blob())) params,
+                         current_user& user) {
+
+    Song song;
+    if (!db.select_by_id<Song>("songs", params.id) >> song)
+      throw error::bad_request("This song's metadata does not exist. Save them before uploading the file.");
+    
+    if (song.user_id != user.id)
+      throw error::unauthorized("This song belongs to another user.");
+
+    std::ofstream f(song_path(song), std::ios::binary);
+    f.write(params.file.data, params.file.len);
+    f.close();      
+  };
+
+  server["stream"] = [] (decltype(D(_Id = int())) params,
+                         sqlite_orm& orm,
+                         response& resp)
+  {
+    Song song;
+    if (!orm.find_by_id(params.id, song))
+      throw error::not_found("This song does not exist.");
+
+    resp.set_header("Content-Type", mime_type(song.filename));
+
+    std::ofstream f(song_path(song), std::ios::binary);
+    char buf[1024];
+    int s;
+    while (s = f.read(buf, sizeof(buf)))
+      resp.write(buf, s);
+    f.close();      
+  };
+
+  server.serve(atoi(argv[2]);
+}
