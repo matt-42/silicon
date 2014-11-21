b2f7c5fe4eaacb13ca3297a150b1d5bc11f65bf0
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2014-11-21 23:40:21 +0100

    Test file upload on request body tail.

---
+++ tests/upload.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+
+#include <iod/sio_utils.hh>
+
+#include <silicon/mimosa_backend.hh>
+#include <silicon/server.hh>
+
+#include "symbols.hh"
+
+using namespace s;
+
+int main(int argc, char* argv[])
+{
+  using namespace iod;
+
+  auto server = silicon();
+
+  server["test_upload"] = [] (request& req)
+  {
+    std::ofstream of("test.txt");
+    stringview content = req.get_tail_string();
+    of.write(content.data(), content.size());
+    of.close();
+  };
+
+  server.serve();
+}
