90ea618da5ab1db65c6de45653fb7d76255d276d
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2014-11-22 19:22:20 +0100

    Server api pretty printing and generation of a simple promise based javascript client.

--- silicon/javascript_client.hh
+++ silicon/javascript_client.hh
@@ -5,60 +5,27 @@ iod_define_symbol(module, _Module);
 
 namespace iod
 {
+  std::string javascript_call_procedure();
 
   template <typename S, typename... T>
   std::string generate_javascript_client(const S& s, T... _options)
   {
-//     std::string prolog = R"json(
+    auto desc = server_api_description(s);
 
-//   var server_url = "http://localhost:8888";
-//   this.call_procedure = function(id, params)
-//   {
-//     // Return a new promise.
-//     var _this = this;
-//     return new Promise(function(resolve, reject) {
-//       // Do the usual XHR stuff
-//       var req = new XMLHttpRequest();
-//       req.open('POST', _this.server_url);
-
-//       req.onload = function() {
-//         // This is called even on 404 etc
-//         // so check the status
-//         if (req.status == 200) {
-//           // Resolve the promise with the response text
-//           resolve(req.response);
-//         }
-//         else {
-//           // Otherwise reject with the status text
-//           // which will hopefully be a meaningful error
-//           reject(Error(req.statusText));
-//         }
-//       };
-
-//       // Handle network errors
-//       req.onerror = function() {
-//         reject(Error("Network Error"));
-//       };
-
-//       // Make the request
-//       body = JSON.stringify({ handler_id: id}) + JSON.stringify(params);
-//       req.send(body);
-//     });
-//   }
-
-
-// )json";
 
     auto opts = D(_options...);
 
     std::ostringstream calls;
 
     auto handlers = s.get_handlers();
-    for (int i = 0; i < handlers.size(); i++)
+    for (int i = 0; i < desc.size(); i++)
     {
-      auto h = handlers[i];
       //visit(h)
-      calls << "  this." << h->name_ << " = function(params) { return this.call_procedure(" << i << ", params); }" << std::endl;
+      calls << "  // ";
+      print_procedure_desc(calls, desc[i]);
+      calls << std::endl;
+      calls << "  this." << desc[i].name << " = function(params) { return this.call_procedure(" << i << ", params); }" << std::endl;
+      calls << std::endl;
     }
 
     std::string module = opts.get(_Module, "sl");
@@ -66,10 +33,54 @@ namespace iod
     std::ostringstream src;
 
     src << "function " << module << "() {" << std::endl;
-    //src << prolog;
+    src << javascript_call_procedure();
     src << calls.str();
-    src << "}";
+    src << "}" << std::endl;
 
     return src.str();
+  }
+  
+  std::string javascript_call_procedure()
+  {
+    return R"javascript(
+  var server_url = "/";
+  this.set_server = function(url) { server_url = url; }
+  this.call_procedure = function(id, params)
+  {
+    // Return a new promise.
+    var _this = this;
+    return new Promise(function(resolve, reject) {
+      // Do the usual XHR stuff
+      var req = new XMLHttpRequest();
+      req.open('POST', _this.server_url);
+
+      req.onload = function() {
+        // This is called even on 404 etc
+        // so check the status
+        if (req.status == 200) {
+          // Resolve the promise with the response text
+          resolve(req.response);
+        }
+        else {
+          // Otherwise reject with the status text
+          // which will hopefully be a meaningful error
+          reject(Error(req.statusText));
+        }
+      };
+
+      // Handle network errors
+      req.onerror = function() {
+        reject(Error("Network Error"));
+      };
+
+      // Make the request
+      body = JSON.stringify({ handler_id: id}) + JSON.stringify(params);
+      req.send(body);
+    });
+  }
+
+
+)javascript";
+    
   }
 }
---
+++ silicon/procedure_desc.hh
@@ -0,0 +1,110 @@
+#pragma once
+
+namespace iod
+{
+  
+  const char* type_string(const void*) { return "void"; }
+  const char* type_string(const std::string*) { return "string"; }
+  const char* type_string(const int*) { return "int"; }
+  const char* type_string(const float*) { return "float"; }
+  const char* type_string(const double*) { return "double"; }
+
+  template <typename... T>
+  std::string type_string(const sio<T...>* o)
+  {
+    std::stringstream res; res << "{";
+    bool first = true;
+    foreach(*o) | [&] (auto& m) {
+      if (!first) res << ", ";
+      first = false;
+      res << m.symbol().name() << ": " << type_string(&m.value());
+    };
+    res << "}";
+    return std::move(res.str());
+  }
+  
+  struct procedure_desc {
+    template <typename H>
+    procedure_desc(H h)
+      : name(h.name_)
+    {
+      // Get the argument list of the handle.
+      typedef callable_return_type_t<typename H::content_type> ret_type;
+      typedef callable_arguments_tuple_t<typename H::content_type> args_type;
+
+      // Extract the sio argument.
+      typedef decltype(foreach(*(args_type*)0) | [&] (auto& a)
+                       {
+                         return static_if<is_sio<decltype(a)>::value>(
+                           [&] () { return &a; },
+                           [&] () { });
+                       }) args2_type;
+
+      fill_args((args2_type*)0);
+      return_type = type_string((ret_type*)0);
+    }
+
+    template <typename T>
+    void fill_args(T*)
+    {
+      typedef std::tuple_element_t<0, T> arg_sio;
+      foreach(*(arg_sio)0) | [&] (auto& a)
+      {
+        args.push_back(std::make_pair(a.symbol().name(), type_string(&a.value())));
+      };
+    }
+
+    void fill_args(void*)
+    {
+    }
+    
+    std::string name;
+    std::vector<std::pair<std::string, std::string>> args;
+    std::string return_type;
+  };
+
+  template <typename S>
+  std::vector<procedure_desc> server_api_description(const S& server)
+  {
+    auto handlers = server.get_handlers();
+    std::vector<procedure_desc> res;
+    for (int i = 0; i < handlers.size(); i++)
+      res.push_back(handlers[i]->description());
+    return std::move(res);
+  }
+
+  template <typename O>
+  void print_procedure_desc(O& os, const procedure_desc& p)
+  {
+    os << p.name << "(";
+    bool first = true;
+    for (auto a : p.args)
+    {
+      if (!first) os << ", ";
+      first = false;
+      os << a.first << ": " << a.second;
+    }
+    os << ") -> " << p.return_type;
+  }
+  
+  template <typename S>
+  void print_server_api(const S& server)
+  {
+    auto desc = server_api_description(server);
+    int i = 0;
+    for (auto p : desc)
+    {
+      std::cout << "procedure " << i << ": " << p.name << "(";
+      bool first = true;
+      for (auto a : p.args)
+      {
+        if (!first) std::cout << ", ";
+        first = false;
+        std::cout << a.first << ": " << a.second;
+      }
+      std::cout << ") -> " << p.return_type << std::endl;;
+      i++;
+    }
+  }
+  
+}
