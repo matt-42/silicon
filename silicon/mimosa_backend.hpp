eb1f8d1fbf92f683d8d7289efd53e2b6a131cdb6
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2014-11-22 19:24:50 +0100

    mimosa backend: Access to the location and take the port as argument.

--- silicon/mimosa_backend.hpp
+++ silicon/mimosa_backend.hpp
@@ -4,21 +4,20 @@ namespace iod
 {
   
   template <typename F>
-  void mimosa_backend::serve(F f)
+  void mimosa_backend::serve(int port, F f)
   {
     mimosa_handler<F> handler(f);
     mh::Server::Ptr server = new mh::Server;
     server->setHandler(&handler);
 
-    if (!server->listenInet4(8888))
+    if (!server->listenInet4(port))
     {
-      std::cerr << "failed to listen on the port " << 8888 << " " << ::strerror(errno) << std::endl;
-      exit(1);
+      std::cerr << "Error: Failed to listen on the port " << port << " " << ::strerror(errno) << std::endl;
+      return;
     }
 
     while(true)
       server->serveOne(0, true);
-    std::cout << "serve end. " << std::endl;
   }
     
 }
