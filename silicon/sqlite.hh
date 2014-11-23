eeb1ff038ab865dc5b2bb246eef02186693214ef
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2014-11-23 19:16:25 +0100

    More constructors for blob.

--- silicon/sqlite.hh
+++ silicon/sqlite.hh
@@ -13,7 +13,9 @@ namespace iod
 
   struct blob : public std::string
   {
+    blob() : std::string() {}
     blob(const std::string& s) : std::string(s) {}
+    blob(const blob& b) : std::string(b) {}
   };
 
   static const struct null_t { null_t() {} } null;
