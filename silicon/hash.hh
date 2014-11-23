af53df5cee0c62517c3635746f144b5e71fbed84
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2014-11-23 15:01:44 +0100

    Add a password hashing function.

---
+++ silicon/hash.hh
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <nettle/sha3.h>
+
+namespace iod
+{
+  inline std::string hash_sha3_512(const std::string& str)
+  {
+    struct sha3_512_ctx ctx;
+    sha3_512_init(&ctx);
+    sha3_512_update(&ctx, str.size(), (const uint8_t*) str.data());
+    uint8_t h[SHA3_512_DIGEST_SIZE];
+    sha3_512_digest(&ctx, sizeof(h), h);
+    return std::string((const char*)h, sizeof(h));
+  }
+}
