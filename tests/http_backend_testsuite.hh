b434ed6084c284b5b465707793e0fe933dc34c0c
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2018-03-19 01:47:59 +0100

    Split backend_testsuite.hh into http/rmq related code.

--- tests/http_backend_testsuite.hh
+++ tests/http_backend_testsuite.hh
@@ -52,43 +52,6 @@ auto hl_api = http_api(
   
 );
 
-std::atomic_int cpt0;
-std::atomic_int cpt1;
-std::atomic_int cpt2;
-std::atomic_int cpt3;
-std::atomic_int cpt4;
-std::atomic_int cpt5;
-
-auto rmq_api = sl::rmq::api(
-  s::_test = [](sl::sqlite_connection &)
-  {
-    std::atomic_fetch_add(&cpt0, 1);
-  },
-  s::_test1 & sl::rmq::parameters(s::_str = std::string()) = [](auto,
-                                                                sl::sqlite_connection &)
-  {
-    std::atomic_fetch_add(&cpt1, 1);
-  },
-  s::_test2 * s::_rk1 = [](sl::sqlite_connection &)
-  {
-    std::atomic_fetch_add(&cpt2, 1);
-  },
-  s::_test3 * s::_rk2 & sl::rmq::parameters(s::_str = std::string()) = [](auto,
-                                                                          sl::sqlite_connection &)
-  {
-    std::atomic_fetch_add(&cpt3, 1);
-  },
-  s::_test4 * s::_rk1 / s::_qn1 = [](sl::sqlite_connection &)
-  {
-    std::atomic_fetch_add(&cpt4, 1);
-  },
-  s::_test5 * s::_rk2 / s::_qn2 & sl::rmq::parameters(s::_str = std::string()) = [](auto,
-                                                                                    sl::sqlite_connection &)
-  {
-    std::atomic_fetch_add(&cpt5, 1);
-  }
-);
-
 template <typename T>
 void backend_testsuite(T port)
 {
